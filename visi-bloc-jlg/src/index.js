db2c721a26d1150aca1dc6cf1136e60ed5eea0c3
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-20 15:39:24 +0200

    Add translatable schedule strings and rebuild assets

---
+++ visi-bloc-jlg/src/index.js
@@ -0,0 +1,394 @@
+/* global VisiBlocData */
+import { Fragment } from '@wordpress/element';
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { BlockControls, InspectorControls } from '@wordpress/block-editor';
+import {
+    ToolbarGroup,
+    ToolbarButton,
+    PanelBody,
+    SelectControl,
+    ToggleControl,
+    CheckboxControl,
+    DateTimePicker,
+} from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { format } from '@wordpress/date';
+import { subscribe, select } from '@wordpress/data';
+
+import './editor-styles.css';
+
+const DEVICE_VISIBILITY_OPTIONS = [
+    {
+        label: __('Visible sur tous les appareils', 'visi-bloc-jlg'),
+        value: 'all',
+    },
+    {
+        label: __('--- Afficher uniquement sur ---', 'visi-bloc-jlg'),
+        value: 'separator-show',
+        disabled: true,
+    },
+    {
+        label: __('Desktop Uniquement', 'visi-bloc-jlg'),
+        value: 'desktop-only',
+    },
+    {
+        label: __('Tablette Uniquement', 'visi-bloc-jlg'),
+        value: 'tablet-only',
+    },
+    {
+        label: __('Mobile Uniquement', 'visi-bloc-jlg'),
+        value: 'mobile-only',
+    },
+    {
+        label: __('--- Cacher sur ---', 'visi-bloc-jlg'),
+        value: 'separator-hide',
+        disabled: true,
+    },
+    {
+        label: __('Caché sur Desktop', 'visi-bloc-jlg'),
+        value: 'hide-on-desktop',
+    },
+    {
+        label: __('Caché sur Tablette', 'visi-bloc-jlg'),
+        value: 'hide-on-tablet',
+    },
+    {
+        label: __('Caché sur Mobile', 'visi-bloc-jlg'),
+        value: 'hide-on-mobile',
+    },
+];
+
+function addVisibilityAttributesToGroup(settings, name) {
+    if (name !== 'core/group') {
+        return settings;
+    }
+
+    settings.attributes = {
+        ...settings.attributes,
+        isHidden: {
+            type: 'boolean',
+            default: false,
+        },
+        deviceVisibility: {
+            type: 'string',
+            default: 'all',
+        },
+        isSchedulingEnabled: {
+            type: 'boolean',
+            default: false,
+        },
+        publishStartDate: {
+            type: 'string',
+        },
+        publishEndDate: {
+            type: 'string',
+        },
+        visibilityRoles: {
+            type: 'array',
+            default: [],
+        },
+    };
+
+    return settings;
+}
+
+const withVisibilityControls = createHigherOrderComponent((BlockEdit) => {
+    return (props) => {
+        if (props.name !== 'core/group') {
+            return <BlockEdit {...props} />;
+        }
+
+        const { attributes, setAttributes, isSelected } = props;
+        const {
+            isHidden,
+            deviceVisibility,
+            isSchedulingEnabled,
+            publishStartDate,
+            publishEndDate,
+            visibilityRoles,
+        } = attributes;
+
+        const onRoleChange = (isChecked, roleSlug) => {
+            const newRoles = isChecked
+                ? [...visibilityRoles, roleSlug]
+                : visibilityRoles.filter((role) => role !== roleSlug);
+
+            setAttributes({
+                visibilityRoles: newRoles,
+            });
+        };
+
+        let scheduleSummary = __('Aucune programmation.', 'visi-bloc-jlg');
+
+        if (isSchedulingEnabled) {
+            const startDate = publishStartDate
+                ? format('d/m/Y H:i', publishStartDate)
+                : null;
+            const endDate = publishEndDate
+                ? format('d/m/Y H:i', publishEndDate)
+                : null;
+
+            if (startDate && endDate) {
+                scheduleSummary = sprintf(
+                    __('Du %s au %s.', 'visi-bloc-jlg'),
+                    startDate,
+                    endDate,
+                );
+            } else if (startDate) {
+                scheduleSummary = sprintf(
+                    __('À partir du %s.', 'visi-bloc-jlg'),
+                    startDate,
+                );
+            } else if (endDate) {
+                scheduleSummary = sprintf(
+                    __('Jusqu\'au %s.', 'visi-bloc-jlg'),
+                    endDate,
+                );
+            } else {
+                scheduleSummary = __('Activée, mais sans date définie.', 'visi-bloc-jlg');
+            }
+        }
+
+        return (
+            <Fragment>
+                <BlockEdit {...props} />
+                {isSelected && (
+                    <Fragment>
+                        <BlockControls>
+                            <ToolbarGroup>
+                                <ToolbarButton
+                                    icon="visibility"
+                                    label={__('Rendre visible', 'visi-bloc-jlg')}
+                                    onClick={() => setAttributes({ isHidden: false })}
+                                    isActive={isHidden === false}
+                                />
+                                <ToolbarButton
+                                    icon="hidden"
+                                    label={__('Rendre caché', 'visi-bloc-jlg')}
+                                    onClick={() => setAttributes({ isHidden: true })}
+                                    isActive={isHidden === true}
+                                />
+                            </ToolbarGroup>
+                        </BlockControls>
+                        <InspectorControls>
+                            <PanelBody
+                                title={__('Contrôles de Visibilité', 'visi-bloc-jlg')}
+                                initialOpen={true}
+                            >
+                                <SelectControl
+                                    label={__('Visibilité par Appareil', 'visi-bloc-jlg')}
+                                    value={deviceVisibility}
+                                    options={DEVICE_VISIBILITY_OPTIONS}
+                                    onChange={(newValue) =>
+                                        setAttributes({ deviceVisibility: newValue })
+                                    }
+                                />
+                            </PanelBody>
+                            <PanelBody
+                                title={__('Programmation', 'visi-bloc-jlg')}
+                                initialOpen={false}
+                                className="visi-bloc-panel-schedule"
+                            >
+                                <ToggleControl
+                                    label={__('Activer la programmation', 'visi-bloc-jlg')}
+                                    checked={isSchedulingEnabled}
+                                    onChange={() =>
+                                        setAttributes({
+                                            isSchedulingEnabled: !isSchedulingEnabled,
+                                        })
+                                    }
+                                />
+                                {isSchedulingEnabled && (
+                                    <div>
+                                        <p
+                                            style={{
+                                                fontStyle: 'italic',
+                                                color: '#555',
+                                            }}
+                                        >
+                                            {scheduleSummary}
+                                        </p>
+                                        <CheckboxControl
+                                            label={__('Définir une date de début', 'visi-bloc-jlg')}
+                                            checked={!!publishStartDate}
+                                            onChange={(isChecked) => {
+                                                setAttributes({
+                                                    publishStartDate: isChecked
+                                                        ? new Date().toISOString()
+                                                        : undefined,
+                                                });
+                                            }}
+                                        />
+                                        {!!publishStartDate && (
+                                            <div className="visi-bloc-datepicker-wrapper">
+                                                <DateTimePicker
+                                                    currentDate={publishStartDate}
+                                                    onChange={(newDate) =>
+                                                        setAttributes({
+                                                            publishStartDate: newDate,
+                                                        })
+                                                    }
+                                                    is12Hour={false}
+                                                />
+                                            </div>
+                                        )}
+                                        <CheckboxControl
+                                            label={__('Définir une date de fin', 'visi-bloc-jlg')}
+                                            checked={!!publishEndDate}
+                                            onChange={(isChecked) => {
+                                                setAttributes({
+                                                    publishEndDate: isChecked
+                                                        ? new Date().toISOString()
+                                                        : undefined,
+                                                });
+                                            }}
+                                        />
+                                        {!!publishEndDate && (
+                                            <div className="visi-bloc-datepicker-wrapper">
+                                                <DateTimePicker
+                                                    currentDate={publishEndDate}
+                                                    onChange={(newDate) =>
+                                                        setAttributes({
+                                                            publishEndDate: newDate,
+                                                        })
+                                                    }
+                                                    is12Hour={false}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
+                            </PanelBody>
+                            <PanelBody
+                                title={__('Visibilité par Rôle', 'visi-bloc-jlg')}
+                                initialOpen={false}
+                            >
+                                <p>
+                                    {__(
+                                        "N'afficher que pour les rôles sélectionnés. Laisser vide pour afficher à tout le monde.",
+                                        'visi-bloc-jlg',
+                                    )}
+                                </p>
+                                <CheckboxControl
+                                    label={__('Visiteurs Déconnectés', 'visi-bloc-jlg')}
+                                    checked={visibilityRoles.includes('logged-out')}
+                                    onChange={(isChecked) =>
+                                        onRoleChange(isChecked, 'logged-out')
+                                    }
+                                />
+                                <CheckboxControl
+                                    label={__('Utilisateurs Connectés (tous)', 'visi-bloc-jlg')}
+                                    checked={visibilityRoles.includes('logged-in')}
+                                    onChange={(isChecked) =>
+                                        onRoleChange(isChecked, 'logged-in')
+                                    }
+                                />
+                                <hr />
+                                {Object.entries(VisiBlocData.roles).map(([slug, name]) => (
+                                    <CheckboxControl
+                                        key={slug}
+                                        label={name}
+                                        checked={visibilityRoles.includes(slug)}
+                                        onChange={(isChecked) =>
+                                            onRoleChange(isChecked, slug)
+                                        }
+                                    />
+                                ))}
+                            </PanelBody>
+                        </InspectorControls>
+                    </Fragment>
+                )}
+            </Fragment>
+        );
+    };
+}, 'withVisibilityControls');
+
+function addEditorCanvasClasses(props, block) {
+    if (block.name !== 'core/group' || !block.attributes) {
+        return props;
+    }
+
+    const { isHidden } = block.attributes;
+    const newClasses = [props.className, isHidden ? 'bloc-editeur-cache' : '']
+        .filter(Boolean)
+        .join(' ');
+
+    return {
+        ...props,
+        className: newClasses,
+    };
+}
+
+function addSaveClasses(extraProps, blockType, attributes) {
+    if (blockType.name !== 'core/group' || !attributes) {
+        return extraProps;
+    }
+
+    const { deviceVisibility } = attributes;
+    const newClasses = [
+        extraProps.className,
+        deviceVisibility && deviceVisibility !== 'all'
+            ? `vb-${deviceVisibility}`
+            : '',
+    ]
+        .filter(Boolean)
+        .join(' ');
+
+    return {
+        ...extraProps,
+        className: newClasses,
+    };
+}
+
+function syncListView() {
+    const { getBlockOrder } = select('core/block-editor');
+    const clientIds = getBlockOrder();
+
+    if (!clientIds.length) {
+        return;
+    }
+
+    clientIds.forEach((clientId) => {
+        const block = select('core/block-editor').getBlock(clientId);
+
+        if (!block || block.name !== 'core/group') {
+            return;
+        }
+
+        const row = document.querySelector(
+            `.block-editor-list-view__block[data-block="${clientId}"]`,
+        );
+
+        if (row) {
+            if (block.attributes.isHidden) {
+                row.classList.add('bloc-editeur-cache');
+            } else {
+                row.classList.remove('bloc-editeur-cache');
+            }
+        }
+    });
+}
+
+addFilter(
+    'blocks.registerBlockType',
+    'visi-bloc-jlg/add-visibility-attributes',
+    addVisibilityAttributesToGroup,
+);
+addFilter(
+    'editor.BlockEdit',
+    'visi-bloc-jlg/with-visibility-controls',
+    withVisibilityControls,
+);
+addFilter(
+    'blocks.getSaveContent.extraProps',
+    'visi-bloc-jlg/add-save-classes',
+    addSaveClasses,
+);
+addFilter(
+    'editor.BlockListBlock.props',
+    'visi-bloc-jlg/add-editor-canvas-classes',
+    addEditorCanvasClasses,
+);
+
+subscribe(syncListView);
