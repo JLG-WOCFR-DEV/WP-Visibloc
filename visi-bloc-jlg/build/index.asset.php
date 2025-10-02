c29e6c4a8700d7eb9f0425622bfa1dc28979194d
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-02 18:02:06 +0200

    Enhance editor styling for hidden blocks

--- visi-bloc-jlg/build/index.asset.php
+++ visi-bloc-jlg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '90db9422f63510c3a5ff');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'edd26c8ceb4df1ff2280');
