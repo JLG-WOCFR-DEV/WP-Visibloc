421376352a879c41079e41c088ffaa500ec95b12
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-14 18:25:09 +0200

    Improve wp-env startup handling for e2e tests

--- visi-bloc-jlg/playwright.config.js
+++ visi-bloc-jlg/playwright.config.js
@@ -1,10 +1,16 @@
 const path = require( 'path' );
 const baseConfig = require( '@wordpress/scripts/config/playwright.config.js' );
 const globalSetup = require.resolve( './tests/e2e/global-setup.js' );
+const startWebServer = require.resolve( './tests/e2e/start-web-server.js' );
 
 module.exports = {
     ...baseConfig,
     testDir: path.resolve( __dirname, 'tests/e2e' ),
     testMatch: [ '**/*.spec.js' ],
     globalSetup,
+    webServer: {
+        ...baseConfig.webServer,
+        command: `node "${ startWebServer }"`,
+        timeout: 240_000,
+    },
 };
